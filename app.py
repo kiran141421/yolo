46c82e008cd2307f55bec49c188a15f0fc1b1513
unknown <kirankumar141421@gmail.com>
2024-04-10 19:47:29 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask 
+from PIL import Image
 from flask import request
 import cv2 as cv
 import base64
+import io
 import numpy as np
 from image_object_detection import yolo_object_detection
 from markupsafe import escape
@@ -17,8 +19,16 @@ def index_page():
     img = request.form['image']
 
     img_bytes = base64.b64decode(img)
-    im_arr = np.frombuffer(img_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
-    img = cv.imdecode(im_arr, flags=cv.IMREAD_COLOR)
+
+    image = Image.open(io.BytesIO(img_bytes))
+
+    # Convert PIL Image to numpy array
+    img_np = np.array(image)
+    img = img_np
+    
+    # im_arr = np.frombuffer(img_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+    # img = cv.imdecode(im_arr, flags=cv.IMREAD_COLOR)
+
     class_names = yolo_object_detection(img)
 
     return str(class_names)
